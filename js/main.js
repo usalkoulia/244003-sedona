7ce2823d69fbb59061d6ab122bd784203c3f0a3a
Ulia Usalko <usalko.ulia@gmail.com>
2017-09-07 23:13:25 +0300

    Закончила джава-скрипт

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 var searchButton = document.querySelector("#search-button");
 var searchForm = document.querySelector(".search-form");
-var buttonShowHotels = document.querySelector(".btn-show");
+var formIn = searchForm.querySelector("#calendar-in");
+var formOut = searchForm.querySelector("#calendar-out");
+var formAdult = searchForm.querySelector("#adult");
+var formChild = searchForm.querySelector("#child");
+
+var storageAdult = localStorage.getItem("adult");
+var storageChild = localStorage.getItem("child");
+
+if (storageAdult) { formAdult.value = storageAdult; }
+if (storageChild) { formChild.value = storageChild; }
 
 searchButton.addEventListener("click", function(evt) {
-  evt.preventDefault(); // вызов метода с названием preventDefault на обьекте evt
-  searchForm.classList.toggle("search-form-show"); // вызов метода с названием toggle на свойстве с названием classList переменной searchForm
+  evt.preventDefault();
+  if (searchForm.classList.contains("search-form-show")) {
+    searchForm.classList.remove("search-form-show");
+    searchForm.classList.remove("search-form-error");
+  } else {
+    searchForm.classList.add("search-form-show");
+  }
 });
 
-buttonShowHotels.addEventListener("click", function(evt) {
-  evt.preventDefault();
+searchForm.addEventListener("submit", function(evt) {
+  if (!formIn.value || !formOut.value || !formAdult.value || !formChild.value) {
+    evt.preventDefault();
+    searchForm.classList.remove("search-form-error");
+    searchForm.offsetWidth = searchForm.offsetWidth;
+    searchForm.classList.add("search-form-error");
+  } else {
+    localStorage.setItem("adult", formAdult.value);
+    localStorage.setItem("child", formChild.value);
+  }
 });
